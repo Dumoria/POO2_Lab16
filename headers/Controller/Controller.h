3c55c9ceef11a62a381a7edd20d2c880a1eda40e
Cloud <benjamin.thomas@heig-vd.ch>
2018-05-17 17:35:49 +0200

    modif controller et vue

--- headers/Controller/Controller.h
+++ headers/Controller/Controller.h
@@ -5,4 +5,74 @@
 #ifndef POO2_LABO4_GOBET_THOMAS_CONTROLLER_H
 #define POO2_LABO4_GOBET_THOMAS_CONTROLLER_H
 
+#include "../Model/Boat.h"
+#include "../Model/Bank.h"
+#include "../View/View.h"
+
+class Controller{
+
+private:
+
+    int turn;
+
+    //Model
+    Bank left;
+    Bank right;
+    Boat boat;
+
+    //View
+    View view;
+
+public:
+
+    Controller controller(){
+
+        showMenu();
+        display();
+
+        char cmd; //cin, vérifier que bien...
+
+        switch(cmd){
+            case 'p':
+                display();
+                break;
+            case 'e':
+
+                break;
+            case 'd':
+
+                break;
+            case 'm':
+
+                break;
+            case 'r':
+                break;
+            case 'q':
+                break;
+            case 'h':
+                break;
+            default:
+                std::cout << "Entrée non valide" << std::endl;
+                break;
+        }
+    }
+
+    void showMenu(){
+        view.menuDisplay();
+    }
+
+    void display();
+    void nextTurn();
+
+    void reinit(){
+        left.reinit();
+        right.reinit();
+        boat.reinit();
+    }
+
+    void quit();
+
+
+};
+
 #endif //POO2_LABO4_GOBET_THOMAS_CONTROLLER_H
--- headers/View/View.h
+++ headers/View/View.h
@@ -5,4 +5,41 @@
 #ifndef POO2_LABO4_GOBET_THOMAS_VIEW_H
 #define POO2_LABO4_GOBET_THOMAS_VIEW_H
 
+#include <string>
+#include <iostream>
+
+const std::string DELIM_LINE = "-----------------------------------------------------------";
+const std::string DELIM_LINE_THICK = "===========================================================";
+const std::string MENU = "p\t: afficher\ne <nom>\t: embarquer <nom>\nd <nom>\t: debarquer <nom>\n"
+"m\t: deplacer bateau\nr\t: reinitialiser\nq\t: quitter\nh\t: menu\n";
+
+class View{
+
+private:
+
+public:
+
+    View view();
+
+    void initialDisplay(){
+        menuDisplay();
+        std::cout << DELIM_LINE << std:: endl;
+        
+    }
+
+    void menuDisplay(){
+        std::cout << MENU << std::endl;
+    }
+
+    void bankDisplay(){
+
+    }
+
+    void boatDisplay(){
+
+    }
+
+};
+
+
 #endif //POO2_LABO4_GOBET_THOMAS_VIEW_H
